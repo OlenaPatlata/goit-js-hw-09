17a7dea959598c2dda7e7ea5bccd108593f45b72
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-02-10 18:08:23 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+// Получаем ссылки на кнопки
+const btnStartRef = document.querySelector('[data-start]');
+const btnStopRef = document.querySelector('[data-stop]');
 
+// Вводим переменную для получения id созданного таймером
+let idInterval = null;
+
+// Функция, которая вызывается при клике по кнопке Start
+const onChangeColor = (event) => {
+    idInterval = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor =`${color}`;
+    btnStartRef.disabled = true;
+    btnStopRef.disabled = false;}, 1000)
+}
+
+// Функция, которая вызывается при клике по кнопке Stop
+const onStopChangeColor = (event) => {
+    clearInterval(idInterval);
+    btnStartRef.disabled = false;
+    btnStopRef.disabled = true;
+}
+// Функция для генерации случайного цвета 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// Вешаем слушателей на кнопки
+btnStartRef.addEventListener("click", onChangeColor);
+btnStopRef.addEventListener("click", onStopChangeColor);
