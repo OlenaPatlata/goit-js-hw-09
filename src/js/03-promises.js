82cbc1a58d638e2bb18661fbb95107792d24907e
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-02-11 13:48:09 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,26 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+  setTimeout(() => {
+   if (shouldResolve) {
+    resolve({ position, delay } )
+  } 
+    reject({ position, delay } )
+  },  delay)
+  })
+  return promise
+};
+  
+
+
+
+function onError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
+function onSuccess({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+createPromise().then(onSuccess).catch(onError);
